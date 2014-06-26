0fc6fa9d7b78a5049799d96cc98d17a8e1bf0bdc
chanakyacool <chanakyacool@github.co>
2014-06-26 13:50:43 +0530

    all changes

--- app/admin/sizetypes.rb
+++ app/admin/sizetypes.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register Sizetype do
 	form do |f|                       
-		f.inputs "Size amd Types" do
-			f.input :size, collection: Size.all.map{|s| [s.size, s.id]}
+		f.inputs "Size and Types" do
+			f.input :company, collection: Company.all.map { |c| [c.company_name, c.id]}
+			f.input :size, collection: Size.all.map{|s| [s.size, s.id] if s.size?}
 			f.input :type, collection: Type.all.map{|t| [t.type_name, t.id]}
 		end
 		f.buttons
@@ -9,7 +10,7 @@ ActiveAdmin.register Sizetype do
  
 	show do |sizetype|
 		attributes_table do
-	        row :id
+	        row :id 
 	       	
 	       	row :size do
 	       		link_to(sizetype.size.size, kickass_size_path(sizetype.size.id))
@@ -23,6 +24,26 @@ ActiveAdmin.register Sizetype do
 	       	row :updated_at
 	    end
 	    active_admin_comments
+	end
+
+	index do
+		selectable_column
+		column :id
+		column "Size" do |m|
+		  cn = Size.find(m.size_id).size
+		  link_to cn, kickass_size_path(m.size)
+		end 
+		column "Type" do |m|
+		  cn = Type.find(m.type_id).type_name
+		  link_to cn, kickass_type_path(m.type)
+		end 
+		# column :type_id
+		# column :company_id
+		column "Company Name" do |m|
+		  cn = Company.find(m.company_id).company_name
+		  link_to cn, kickass_company_path(m.company)
+		end 
+		actions
 	end
 end
  
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
 	def index
 			
 	end
+	def self.findcompany
+		find_all_by_company_id
+	end
 end
