38a09e1878876c8bcfec7a986ab58d3fd2cd2978
chanakyacool <chanakyacool@github.co>
2014-06-26 16:12:31 +0530

    sizetype js

--- app/admin/sizetypes.rb
+++ app/admin/sizetypes.rb
@@ -1,10 +1,33 @@
-ActiveAdmin.register Sizetype do
+ActiveAdmin.register Sizetype do   
 	form do |f|                       
 		f.inputs "Size and Types" do
-			f.input :company, collection: Company.all.map { |c| [c.company_name, c.id]}
-			f.input :size, collection: Size.all.map{|s| [s.size, s.id] if s.size?}
-			f.input :type, collection: Type.all.map{|t| [t.type_name, t.id]}
-		end
+			f.input :company,:input_html => {
+												:onchange => "  
+												var company = $(this).val();
+
+												$('#sizetype_size_input').val(0).find('option').each(function(){
+												var $option = $(this),
+												isCorrectType = ($option.attr('data-user') === company);  
+
+												$option.prop('disabled',!isCorrectType);
+												});
+											  "
+											}, collection: Company.all.map{|t| [t.company_name, t.id]}
+
+			f.input :size, collection: Size.all.map{|s| [s.size, s.id, {"data-user" => s.company_id}]  }
+			# :input_html => {  	
+			# 									:onchange => "  var user = $(this).val();
+			# 									$('#sizetype_type_id').val(0).find('option').each(function(){
+			# 									var $option = $(this),
+			# 									isCorrectUser = ($option.attr('data-user') === user);  
+
+			# 									$option.prop('disabled',!isCorrectUser);
+			# 									});
+			# 								  "
+			# 								}, collection: Size.all.map{|s| [s.size, s.id, {"data-user" => s.company_id}]  }
+			f.input :type, collection: Type.all.map{|s| [s.type_name, s.id]   }
+			# f.input :type, collection: Type.all.map{|s| [s.type_name, s.id, {"data-user" => s.sizes}]   }
+			 end
 		f.buttons
 	end
  
@@ -29,20 +52,20 @@ ActiveAdmin.register Sizetype do
 	index do
 		selectable_column
 		column :id
-		column "Size" do |m|
-		  cn = Size.find(m.size_id).size
-		  link_to cn, kickass_size_path(m.size)
+		column "Company Name" do |m|
+		  cn = Company.find(m.company_id).company_name
+		  link_to cn, kickass_company_path(m.company)
 		end 
 		column "Type" do |m|
 		  cn = Type.find(m.type_id).type_name
 		  link_to cn, kickass_type_path(m.type)
 		end 
+		column "Size" do |m|
+		  cn = Size.find(m.size_id).size
+		  link_to cn, kickass_size_path(m.size)
+		end 
 		# column :type_id
 		# column :company_id
-		column "Company Name" do |m|
-		  cn = Company.find(m.company_id).company_name
-		  link_to cn, kickass_company_path(m.company)
-		end 
 		actions
 	end
 end
